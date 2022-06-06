3d9b266ee21c51b1019cb3aeab9daf77e7cf9dc6
Anuar2 <orazbekov.anvar2@gmail.com>
2022-06-06 23:36:30 +0600

    practice animation

---
+++ AnimationApplication/Extension + UIButton.swift
@@ -0,0 +1,24 @@
+//
+//  Extension + UIButton.swift
+//  AnimationApplication
+//
+//  Created by Anuar Orazbekov on 06.06.2022.
+//
+
+import UIKit
+
+extension UIButton {
+    func pulsate() {
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.fromValue = 0.95
+        pulse.toValue = 1
+        pulse.duration = 0.6
+        pulse.autoreverses = true
+        pulse.repeatCount = 2
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1
+        
+        
+        layer.add(pulse, forKey: nil)
+    }
+}
--- AnimationApplication/ViewController.swift
+++ AnimationApplication/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var coreAnimationView: UIView!
+    @IBOutlet var springAnimationView: SpringView!
+    
+    private var originCordinate: CGFloat?
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        originCordinate = coreAnimationView.frame.origin.x
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        originCordinate = coreAnimationView.frame.origin.x
+        
     }
-
-
+    
+    @IBAction func coreAnimationButtonPressed(_ sender: UIButton) {
+        sender.pulsate()
+        
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       options: [.autoreverse, .repeat]){ [weak self] in
+            guard let self = self else {return}
+        
+            if self.coreAnimationView.frame.origin.x == self.originCordinate{
+                self.coreAnimationView.frame.origin.x -= 20
+            }
+        }
+  
+        
+        if coreAnimationView.frame.origin.x == originCordinate{
+        coreAnimationView.frame.origin.x -= 20
+    }
+    }
+    
+    @IBAction func spingAnimationButtonPressed(_ sender: SpringButton) {
+        springAnimationView.animation = Spring.AnimationPreset.slideLeft.rawValue
+        springAnimationView.curve = Spring.AnimationCurve.easeIn.rawValue
+        springAnimationView.duration = 1
+        springAnimationView.animate()
+        
+        sender.animation = "shake"
+        sender.animate()
+    }
+    
+    
 }
 
